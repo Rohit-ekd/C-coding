2c22f029aad030b26ded24e39a7c407b7964d3d3
Rohit-ekd <136239562+Rohit-ekd@users.noreply.github.com>
2023-06-12 12:07:02 +0530

    Add files via upload

---
+++ type.js
@@ -0,0 +1,8 @@
+function myFunction()
+{
+var str = document.getElementById('source').value;
+
+var len = str.length;
+    document.getElementById("result").innerHTML="<textarea>"+len+"</textarea>";
+
+}
